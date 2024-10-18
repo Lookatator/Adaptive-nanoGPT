f1037a5b70fd2a91efd054295078dfe3eb6c3170
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2024-10-18 22:17:57 +0000

    adding config enwik8

--- config/train_enwik8.py
+++ config/train_enwik8.py
@@ -1,27 +1,26 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare-char'
+out_dir = 'out-enwik8-char'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
 
-# we expect to overfit on this small dataset, so only save when val improves
-always_save_checkpoint = False
+always_save_checkpoint = True
 
-wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
+wandb_log = True # override via command line if you like
+wandb_project = 'enwik8-char'
 wandb_run_name = 'nano-gpt'
 
-dataset = 'shakespeare_char'
+dataset = 'enwik8'
 gradient_accumulation_steps = 1
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 6
-n_head = 6
-n_embd = 384
+n_head = 12
+n_embd = 768
 dropout = 0.2
 flash = False
 
