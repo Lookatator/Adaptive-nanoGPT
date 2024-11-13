aa081ead18cc81e5a8990c088736bd1af625d99e
Luca Grillotti <luca.grillotti16@imperial.ac.uk>
2024-11-13 10:53:45 +0000

    new config for new lr properties and more attention heads

--- config/train_enwik8.py
+++ config/train_enwik8.py
@@ -14,15 +14,19 @@ block_size = 512 # context of up to 512 previous characters
 
 # baby GPT model :)
 n_layer = 12
-n_head = 2
+n_head = 8
 n_embd = 512
 dropout = 0.2
 flash = True
 
+max_iters = 200000
+
+# Learning rate
 learning_rate = 1e-3
-max_iters = 100000
-lr_decay_iters = 100000 # make equal to max_iters usually
-min_lr = 3e-4 # learning_rate / 10 usually
+decay_lr = True
+lr_decay_iters = 200000 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
+beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
